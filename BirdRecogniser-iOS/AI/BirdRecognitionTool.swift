74a30897b7a4c1c3b8695c14c153f64a24343b9f
zongzheng <zongzheng@me.com>
2023-07-30 01:09:41 +1200

    remove alamofire

--- BirdRecogniser-iOS/AI/BirdRecognitionTool.swift
+++ BirdRecogniser-iOS/AI/BirdRecognitionTool.swift
@@ -8,7 +8,6 @@
 import Foundation
 import UIKit
 import CoreML
-import Alamofire
 
 struct BirdRecognisitionResult: Equatable, Hashable {
     
--- BirdRecogniser-iOS/UI/Recogniser/RecogniseResultsView.swift
+++ BirdRecogniser-iOS/UI/Recogniser/RecogniseResultsView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import Alamofire
 
 struct RecogniseResultsView: View {
     @Environment(\.presentationMode) var presentationMode
